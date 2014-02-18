1ff70bbc3fe65a938f0d4eb2333ee030f72dc9b6
prakashkharche <prakashkharche@gmail.com>
2014-02-18 14:05:59 +0530

    ny

--- HelloWorld/src/com/hello/demo.java
+++ HelloWorld/src/com/hello/demo.java
@@ -8,7 +8,8 @@ public class demo {
 	public static void main(String[] args) {
 	System.out.println("prskash is gandu.");
 	System.out.println("It is Awesome");
-
+	int a = 2;
+	System.out.println(a);
 	}
 
 }
